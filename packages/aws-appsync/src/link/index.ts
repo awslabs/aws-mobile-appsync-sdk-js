93f36311287fd7064f46be8f0600b4d68735e0ff
Manuel Iglesias <manuelig@amazon.com>
2018-08-14 16:04:04 -0700

    Create generic non terminating link

--- packages/aws-appsync/src/link/index.ts
+++ packages/aws-appsync/src/link/index.ts
@@ -10,5 +10,6 @@ export { AuthLink, AUTH_TYPE } from './auth-link';
 export { OfflineLink, saveSnapshot, replaceUsingMap, saveServerId } from './offline-link';
 export { SubscriptionHandshakeLink } from "./subscription-handshake-link";
 export { NonTerminatingHttpLink } from "./non-terminating-http-link";
+export { NonTerminatingLink } from "./non-terminating-link";
 export { ComplexObjectLink } from "./complex-object-link";
 export { Signer } from "./signer";
--- packages/aws-appsync/src/link/non-terminating-http-link.ts
+++ packages/aws-appsync/src/link/non-terminating-http-link.ts
@@ -1,41 +1,16 @@
 /*!
- * Copyright 2017-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2017-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * Licensed under the Amazon Software License (the "License"). You may not use this file except in compliance with the License. A copy of 
  * the License is located at
  *     http://aws.amazon.com/asl/
  * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY 
  * KIND, express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
-import { ApolloLink, Observable } from 'apollo-link';
-import { setContext } from 'apollo-link-context';
-import { createHttpLink } from 'apollo-link-http';
-import { ExecutionResult } from 'graphql';
+import { createHttpLink, FetchOptions } from 'apollo-link-http';
+import { NonTerminatingLink } from './non-terminating-link';
 
-export class NonTerminatingHttpLink extends ApolloLink {
-
-    contextKey;
-    /** @type {ApolloLink} */
-    link;
-
-    constructor(contextKey, options) {
-        super();
-        this.contextKey = contextKey;
-        this.link = options.resultsFetcherLink;
-    }
-
-    request(operation, forward) {
-        return setContext(async (request, prevContext) => {
-            const result = await new Promise((resolve, reject) => {
-                this.link.request(operation).subscribe({
-                    next: resolve,
-                    error: reject,
-                });
-            });
-
-            return {
-                ...prevContext,
-                [this.contextKey]: result,
-            }
-        }).request(operation, forward);
+export class NonTerminatingHttpLink extends NonTerminatingLink {
+    constructor(contextKey: string, options: FetchOptions) {
+        super(contextKey, { link: createHttpLink(options) });
     }
 }
