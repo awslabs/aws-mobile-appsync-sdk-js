0f48dfdb7042deb584c37e584dd8055c0aeb6ab5
Manuel Iglesias <manuelig@amazon.com>
2018-12-11 12:04:40 -0800

    Switch to debug@2.6.9

--- packages/aws-appsync/src/utils/logger.ts
+++ packages/aws-appsync/src/utils/logger.ts
@@ -4,6 +4,25 @@ export type Logger = Function & {
     extend(category: string): Logger;
 };
 
-const logger: Logger = debug('aws-appsync');
+const noLogger: Logger = (() => {
+    const logger = console.log.bind(console, 'aws-appsync');
+    logger.extend = (_category: string) => logger;
+
+    return logger;
+})();
+
+const debugLogger = debug('aws-appsync') as Logger;
+
+const extend = function (category = '') {
+    const newCategory = category ? [...this.namespace.split(':'), category].join(':') : this.namespace;
+
+    const result = debug(newCategory);
+    result.extend = extend.bind(result);
+
+    return result;
+};
+debugLogger.extend = extend.bind(debugLogger);
+
+const logger: Logger = debug ? debugLogger : noLogger;
 
 export default logger;
