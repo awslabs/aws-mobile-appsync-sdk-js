0d8505728d2ccf0d96221c926d4751ccb5607f0d
boxcc <lmfmcbo@gmail.com>
2020-07-31 14:13:50 +0800

    chore: remove space

--- packages/aws-appsync-auth-link/src/index.ts
+++ packages/aws-appsync-auth-link/src/index.ts
@@ -1,6 +1,5 @@
 import { AuthLink, AuthOptions, AUTH_TYPE, USER_AGENT_HEADER, USER_AGENT } from './auth-link';
 
-
 export const createAuthLink = ({ url, region, auth }: { url: string, region: string, auth: AuthOptions }) => new AuthLink({ url, region, auth });
 
 export { AuthLink, AuthOptions, AUTH_TYPE, USER_AGENT_HEADER, USER_AGENT };
