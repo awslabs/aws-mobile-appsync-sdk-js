d1b909cc25cdff5bbd0a0b292a180466673a3192
Pranav Malewadkar <7400617+pranavosu@users.noreply.github.com>
2025-09-29 15:22:23 -0700

    feat: support ESM + CJS

---
+++ packages/aws-appsync-auth-link/tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  clean: true,
+  outDir: 'lib',
+  external: [
+    '@apollo/client',
+    '@aws-crypto/sha256-js',
+    '@aws-sdk/types',
+    '@aws-sdk/util-hex-encoding',
+    'debug',
+    'rxjs'
+  ]
+})
---
+++ packages/aws-appsync-subscription-link/tsup.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  dts: true,
+  clean: true,
+  outDir: "lib",
+  external: [
+    "@apollo/client",
+    "@redux-offline/redux-offline",
+    "aws-appsync-auth-link",
+    "debug",
+    "rxjs",
+    "url",
+    "zen-observable-ts",
+  ],
+  onSuccess: "cp -r src/vendor lib/vendor",
+});
